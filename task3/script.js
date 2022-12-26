8d1b4fd4ccbf7c6db82298ac3d31838a818be7e1
NikulinSA <sergeyniky@mail.ru>
2022-12-26 22:52:58 +0300

    fix

--- task3/script.js
+++ task3/script.js
@@ -1,21 +1,32 @@
 const trafficLight = document.querySelector('#trafficLight');
+// let i;
 
 function makeGreen() {
+    // for(i = 0; i < trafficLight.length; ++i){
     trafficLight.style.background = ('green');
     trafficLight.removeEventListener('click', makeGreen);
     trafficLight.addEventListener('click', makeYellow);
+    // }
 }
 
 function makeYellow() {
+    // for(i = 0; i < trafficLight.length; ++i){
     trafficLight.style.background = ('yellow');
     trafficLight.removeEventListener('click', makeYellow);
     trafficLight.addEventListener('click', makeRed);
+    // }
 }
 
 function makeRed() {
+    // for(i = 0; i < trafficLight.length; ++i){
     trafficLight.style.background = ('red');
     trafficLight.removeEventListener('click', makeRed);
     trafficLight.addEventListener('click', makeGreen);
+    // }
 }
 
-trafficLight.addEventListener('click', makeGreen);
+
+//  for(i = 0; i < trafficLight.length; ++i){
+ trafficLight.addEventListener('click', makeGreen);
+//  }
+
